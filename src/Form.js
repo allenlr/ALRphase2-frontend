615798645faf19bc5d569618b7d6546339fdd923
allen russ <allenlru55@gmail.com>
2023-02-20 17:32:24 -0700

    add basic form with inputs, button, and labels

--- src/Form.js
+++ src/Form.js
@@ -2,11 +2,21 @@ import React from 'react';
 
 function Form(){
     return (
-        <div>
-            {/* <label>New Coin Name</label>
-                <input type="text">Name</input>
+        <div style={{
+            color: "white",
+        }}>
+            <h3>Add your own project!</h3>
+            <label>New Coin Name</label>
+            <p>
+                <input type="text"></input>
+            </p>
             <label>TICKER</label>
-                <input type="text">ticker</input> */}
+            <p>
+                <input type="text"></input>
+            </p>
+            <p>
+                <button type="submit">Add Coin</button>
+            </p>
         </div>
     )
 }
